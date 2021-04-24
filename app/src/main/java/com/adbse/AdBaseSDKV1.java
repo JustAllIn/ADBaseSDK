0501719c69916d6cd7fc27e28711baf5b0727747
Hua <zhonghua.1993@163.com>
2021-04-24 23:12:19 +0800

    [adbase sdk] 一些基础的网络请求调用逻辑

--- app/src/main/java/com/adbse/AdBaseSDKV1.java
+++ app/src/main/java/com/adbse/AdBaseSDKV1.java
@@ -1,10 +1,16 @@
 package com.adbse;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public enum  AdBaseSDKV1 implements IAdBaseSDK{
 
     Instance;
 
     private final ApiProxy mApiProxy;
+
+    private String alive_id;    // TODO: 2021/4/24 在open接口请求成功后返回并缓存
 
 
     AdBaseSDKV1() {
@@ -13,31 +19,122 @@ public enum  AdBaseSDKV1 implements IAdBaseSDK{
 
     @Override
     public int open() {
+        mApiProxy.open(getSeatId(),getAppCRC())
+                .enqueue(new Callback<String>() {
+                    @Override
+                    public void onResponse(Call<String> call, Response<String> response) {
+                       alive_id  =  response.body();    // TODO: 2021/4/24 确定不需要解析吗
+                    }
+
+                    @Override
+                    public void onFailure(Call<String> call, Throwable t) {
+
+                    }
+                });
         return 0;
     }
 
     @Override
     public int heartbeat() {
+        mApiProxy.heartBeat(alive_id)
+                .enqueue(new Callback<String>() {
+                    @Override
+                    public void onResponse(Call<String> call, Response<String> response) {
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<String> call, Throwable t) {
+
+                    }
+                });
         return 0;
     }
 
     @Override
     public int join() {
+        final String type = ""; // TODO: 2021/4/24 ?
+        final String name = ""; // TODO: 2021/4/24 ?
+
+        mApiProxy.join(alive_id,type,name)
+                .enqueue(new Callback<String>() {
+                    @Override
+                    public void onResponse(Call<String> call, Response<String> response) {
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<String> call, Throwable t) {
+
+                    }
+                });
         return 0;
     }
 
     @Override
     public int login() {
+        final String type = ""; // TODO: 2021/4/24 ?
+        final String name = ""; // TODO: 2021/4/24 ?
+
+        mApiProxy.login(alive_id,type,name)
+                .enqueue(new Callback<String>() {
+                    @Override
+                    public void onResponse(Call<String> call, Response<String> response) {
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<String> call, Throwable t) {
+
+                    }
+                });
         return 0;
     }
 
     @Override
     public int logout() {
+        final String type = ""; // TODO: 2021/4/24 ?
+        final String name = ""; // TODO: 2021/4/24 ?
+        mApiProxy.login(alive_id,type,name)
+                .enqueue(new Callback<String>() {
+                    @Override
+                    public void onResponse(Call<String> call, Response<String> response) {
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<String> call, Throwable t) {
+
+                    }
+                });
         return 0;
     }
 
     @Override
     public int exit() {
+        mApiProxy.exit(alive_id)
+                .enqueue(new Callback<String>() {
+                    @Override
+                    public void onResponse(Call<String> call, Response<String> response) {
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<String> call, Throwable t) {
+
+                    }
+                });
         return 0;
+    }
+
+
+    private String getAppCRC() {
+        // TODO: 2021/4/24 从哪儿来
+        return null;
+    }
+
+    private String getSeatId() {
+        // TODO: 2021/4/24 从哪儿来
+        return null;
     }
 }
